04eaf4cd8399d42d8295a501dfb98fd7ae5e3b40
jiwook <xodhks_0113@naver.com>
2018-08-30 17:59:26 +0900

    썸네일 작업중.

---
+++ PDFKitSample/ThumCell.swift
@@ -0,0 +1,24 @@
+//
+//  ThumCell.swift
+//  PDFKitSample
+//
+//  Created by JW_Macbook on 2018. 8. 30..
+//  Copyright © 2018년 JW_Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ThumCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- PDFKitSample/ViewController.swift
+++ PDFKitSample/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     // Sample PDFView(UIView)
     @IBOutlet weak var pdfView: PDFView!
     
+    // Sample ThumtableView
+    @IBOutlet weak var thumTableView: UITableView!
+    
+    var pdfThumArray:NSMutableArray = NSMutableArray()
+    var localPdfDocument:PDFDocument?
     
     /// PDFView Setting..
     func loadPdfView() {
@@ -22,16 +27,21 @@ class ViewController: UIViewController {
         // Sample Pdf File..
         // web Sample Pdf
         // http://gahp.net/wp-content/uploads/2017/09/sample.pdf
-        let url = URL(string: "http://gahp.net/wp-content/uploads/2017/09/sample.pdf")
+//        let url = URL(string: "http://gahp.net/wp-content/uploads/2017/09/sample.pdf")
         
         // local Sample Pdf
-//        let url = Bundle.main.url(forResource: "genesis-catalogue", withExtension: "pdf")
+        let url = Bundle.main.url(forResource: "genesis-catalogue", withExtension: "pdf")
         
         if let pdfDocument = PDFDocument(url: url!) {
             pdfView.autoScales = true
             pdfView.displayMode = .singlePageContinuous
             pdfView.displayDirection = .vertical
             pdfView.document = pdfDocument
+            
+            // Thum Image..
+            localPdfDocument = pdfDocument
+            self.view.bringSubview(toFront: self.thumTableView)
+            self.thumTableView.reloadData()
         }
     }
     
@@ -45,7 +55,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
 
+// MARK: - UITablView Delegate, DataSource Extention
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return localPdfDocument?.pageCount ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let page = localPdfDocument?.page(at: indexPath.row)
+        return UITableViewCell()
+    }
+    
+    
 }
 
