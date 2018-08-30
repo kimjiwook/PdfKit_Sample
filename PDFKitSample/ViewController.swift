336d03a97a2365107f6a21c1aa4a8d1059c37ec1
jiwook <xodhks_0113@naver.com>
2018-08-30 16:57:52 +0900

    PdfViewing Complete...

--- PDFKitSample/ViewController.swift
+++ PDFKitSample/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
 
+    // Sample PDFView(UIView)
+    @IBOutlet weak var pdfView: PDFView!
+    
+    
+    /// PDFView Setting..
+    func loadPdfView() {
+        // 1. Main PdfViewer
+        
+        // Sample Pdf File..
+        // web Sample Pdf
+        // http://gahp.net/wp-content/uploads/2017/09/sample.pdf
+        let url = URL(string: "http://gahp.net/wp-content/uploads/2017/09/sample.pdf")
+        
+        // local Sample Pdf
+//        let url = Bundle.main.url(forResource: "genesis-catalogue", withExtension: "pdf")
+        
+        if let pdfDocument = PDFDocument(url: url!) {
+            pdfView.autoScales = true
+            pdfView.displayMode = .singlePageContinuous
+            pdfView.displayDirection = .vertical
+            pdfView.document = pdfDocument
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.loadPdfView()
     }
 
     override func didReceiveMemoryWarning() {
